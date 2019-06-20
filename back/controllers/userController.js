64296dc28c85249385c1fb19fbad6c00597ce632
aiste.vizgirdaite <aiste.vizgirdaitee@gmail.com>
2019-06-20 21:38:45 +0300

    create index.html

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,12 +1,32 @@
-<<<<<<< HEAD
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { superSec } = require("../config/config");
+
+//Register function for UserModel
+let register = (req, res) => {
+  let data = req.body;
+  let user = new User();
+  user.userName = data.userName;
+  user.email = data.email;
+  user.password = data.password;
+  user
+    .save()
+    .then(item => {
+      res.json(item);
+    })
+    .catch(e => res.status(400).json(e));
+};
+
+//Login function for userModel
 let login = (req, res) => {
   let data = req.body;
   User.findOne({
-    email: data.email
+    userName: data.userName
   })
     .then(user => {
       if (!user) {
-        res.json("No user with this email");
+        res.json("No user with this username");
         return;
       }
       bcrypt.compare(data.password, user.password, (err, response) => {
@@ -18,7 +38,7 @@ let login = (req, res) => {
                 _id: user._id.toHexString(),
                 access
               },
-              secretSalt
+              superSec
             )
             .toString();
           user.tokens.push({
@@ -35,82 +55,26 @@ let login = (req, res) => {
     })
     .catch(e => res.status(400).json(e));
 };
-=======
-const User = require('../models/userModel')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {
-    superSec
-} = require('../config/config')
-
-//Register function for UserModel
-let register = (req, res) => {
-    let data = req.body
-    let user = new User()
-    user.userName = data.userName
-    user.email = data.email
-    user.password = data.password
-    user.save()
-        .then((item) => {
-            res.json(item)
-        })
-        .catch(e => res.status(400).json(e))
-}
-
-//Login function for userModel
-let login = (req, res) => {
-    let data = req.body
-    User.findOne({
-            userName: data.userName
-        })
-        .then((user) => {
-            if (!user) {
-                res.json("No user with this username")
-                return
-            }
-            bcrypt.compare(data.password, user.password, (err, response) => {
-                if (response) {
-                    let access = 'auth'
-                    let token = jwt.sign({
-                        _id: user._id.toHexString(),
-                        access
-                    }, superSec).toString()
-                    user.tokens.push({
-                        access,
-                        token
-                    })
-                    user.save()
-                        .then((useris) => {
-                            res.header('x-auth', token).json(useris)
-                        })
-                } else {
-                    res.json('incorrect password')
-                }
-            })
-
-        })
-        .catch(e => res.status(400).json(e))
-}
 
 let logout = (req, res) => {
-    let user = req.user
-    let token = req.token
-    user.update({
-        $pull: {
-            tokens: {
-                token
-            }
+  let user = req.user;
+  let token = req.token;
+  user
+    .update({
+      $pull: {
+        tokens: {
+          token
         }
-    }).then(() => {
-        res.json('logged out')
-    }).catch(e => res.status(400).json(e))
-}
-
-
+      }
+    })
+    .then(() => {
+      res.json("logged out");
+    })
+    .catch(e => res.status(400).json(e));
+};
 
 module.exports = {
-    register,
-    login,
-    logout
-}
->>>>>>> 1e199975c71c1e977981d59a0d4637d91b9a2967
+  register,
+  login,
+  logout
+};
