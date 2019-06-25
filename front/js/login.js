a0102741ecc7ee2d9568d00729868b75a08d1f34
Lapeikutis <kristupas.lapeika@gmail.com>
2019-06-25 19:20:09 +0300

    Login Actual fix

--- front/js/login.js
+++ front/js/login.js
@@ -1,33 +1,37 @@
-function loginUser() {
-  let uName = document.getElementById("userName").value;
-  let pass = document.getElementById("password").value;
+function loginUser(req, res) {
+    let uName = document.getElementById("userName").value;
+    let pass = document.getElementById("password").value;
 
-  alert('here');
-  fetch('http://localhost:3000/api/login', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        userName: uName,
-        password: pass
-      })
-    })
-    .then(res => {
-      localStorage.setItem('website-x-auth-token', res.headers.get('x-auth'))
-      return res.json()
-    })
-    .then(data => {
-      location.replace("http://localhost:8080/index.html")
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    // alert('here');
+    fetch('http://localhost:3000/api/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userName: uName,
+                password: pass
+            })
+        })
+        .then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
+            localStorage.setItem('website-x-auth-token', res.headers.get('x-auth'))
+            return res.json()
+        })
+        .then(data => {
+            console.log(data);
+            location.replace("http://localhost:8080/index.html")
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
 }
 document.getElementById('login').addEventListener('click', () => loginUser());
 
 document.getElementById('register').addEventListener('click', () => {
-  location.replace("http://localhost:8080/register.html");
+    location.replace("http://localhost:8080/register.html");
 })
