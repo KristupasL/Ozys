c3a0c10b082b8652144f71cb90b18534466226ce
TautvydasRudis <r_tautvydas@yahoo.com>
2019-06-25 20:18:54 +0300

    basic index.html creating

--- front/js/login.js
+++ front/js/login.js
@@ -1,8 +1,16 @@
+document.getElementById('login').addEventListener('click', () => {
+  loginUser()
+});
+
+document.getElementById('register').addEventListener('click', () => {
+  location.replace("http://localhost:8080/register.html");
+});
+
+
 function loginUser() {
-  let uName = document.getElementById("userName").value;
+  let userName = document.getElementById("userName").value;
   let pass = document.getElementById("password").value;
 
-  alert('here');
   fetch('http://localhost:3000/api/login', {
       method: 'POST',
       headers: {
@@ -10,11 +18,10 @@ function loginUser() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        userName: uName,
+        userName: userName,
         password: pass
       })
-    })
-    .then(res => {
+    }).then(res => {
       localStorage.setItem('website-x-auth-token', res.headers.get('x-auth'))
       return res.json()
     })
@@ -26,8 +33,3 @@ function loginUser() {
     })
 
 }
-document.getElementById('login').addEventListener('click', () => loginUser());
-
-document.getElementById('register').addEventListener('click', () => {
-  location.replace("http://localhost:8080/register.html");
-})
