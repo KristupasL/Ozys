1113843bedbfdfdae63e4a7e03c6516cf71fd444
Lapeikutis <kristupas.lapeika@gmail.com>
2019-06-26 19:48:34 +0300

    Post&User Models update, likes should work now

--- back/controllers/feedController.js
+++ back/controllers/feedController.js
@@ -2,11 +2,24 @@ const PostModel = require("../models/postModel.js")
 
 let feedLoad = (req, res) => {
     PostModel
-        .find({})
+        .find({}, {
+            photo: 1,
+            creator: 1,
+            likesCount: 1,
+            commentsCount: 1,
+            date: 1,
+            title: 1,
+            likesList: {
+                $elemMatch: {
+                    $eq: req.user._id
+                }
+            }
+        })
         .limit(10)
         .sort({
             date: 'desc'
         })
+        .populate('creator', 'userName photo')
         .then(items => {
             res.json(items);
         })
