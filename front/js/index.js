ebfde8620ac11057ac1862e418176ae2300575a8
pashaGhub <pawelsucharew@gmail.com>
2019-07-01 19:31:37 +0300

    index.js refactoring & post btns added

--- front/js/index.js
+++ front/js/index.js
@@ -66,110 +66,100 @@ function createPost(feedPosts) {
     console.log((value.date));
 
     let post = document.createElement('div')
+    let postAuthorSection = document.createElement('div')
+    let postAuthorSectionLogo = document.createElement('img')
+    let postAuthorSectionName = document.createElement('span')
+    let postAuthorSectionPostDate = document.createElement('span')
+    let postPhoto = document.createElement('img')
+    let postActionButtons = document.createElement('div')   //mygtuku divas
+    let postActionButtonsLike = document.createElement('button')
+    let postActionButtonsComment = document.createElement('button')
+    let postActionButtonsShare = document.createElement('button')
+    let postLikeCount = document.createElement('div')
+    let postComments = document.createElement('div')
+    let postCommentsList = document.createElement('ul')
+    let postCommentsListItem = document.createElement('li')
+    let postCommentsListItemAuthorName = document.createElement('span')
+    let postCommentsListItemAuthorComment = document.createElement('span')
+    let postCommentsListItem2 = document.createElement('li')
+    let postCommentsListItem2Username2 = document.createElement('span')
+    let postCommentsListItem2Username2Text2 = document.createElement('span')
+    let postCommentsListItem3 = document.createElement('li')
+    let postCommentsListItem3Username3 = document.createElement('span')
+    let postCommentsListItem3Username3Text3 = document.createElement('span')
+    let postCreateComment = document.createElement('div')
+    let postCreateCommentInput = document.createElement('input')
+    let postCreateCommentButton = document.createElement('button')
+
     post.classList.add('post', 'list-group-item')   //cia klase uzdeta tik del css borderio, kad graziau atrodytu
+    postAuthorSection.classList.add('post__author-section')
+    postAuthorSectionLogo.classList.add('post__author-section__logo')
+    postAuthorSectionName.classList.add('post__author-section__name')
+    postAuthorSectionPostDate.classList.add('post__author-section__postDate')
+    postActionButtons.classList.add('post__action-buttons')
+    postActionButtonsLike.classList.add('post__action-buttons__item')
+    postActionButtonsLike.classList.add('like')
+    postActionButtonsComment.classList.add('post__action-buttons__item')
+    postActionButtonsComment.classList.add('comment')
+    postActionButtonsShare.classList.add('post__action-buttons__item')
+    postActionButtonsShare.classList.add('share')
+    postLikeCount.classList.add('post__like-count')
+    postComments.classList.add('post__comments')
+    postCommentsList.classList.add('post__comments__list')
+    postCommentsListItem.classList.add('post__comments__list__item')
+    postCreateComment.classList.add('post__create-comment-section', 'col', 'col-md-8')
+    postCreateCommentInput.classList.add('post__create-comment-section__input')
+    postCreateCommentButton.classList.add('post__create-comment-section__button')
 
-    let userSection = document.createElement('div')
-    userSection.classList.add('userSection')
+    postAuthorSectionLogo.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzgv1fJtdWd-9_XAxYs5LeMewuX5USnZZ432pPfdmVGd5_9N6hSQ"); //posto autoriaus logo
+    postAuthorSectionLogo.setAttribute("height", "50");
+    postPhoto.setAttribute("src", value.photo);   //posto autoriaus posto nuotrauka
+    postPhoto.setAttribute("width", "100%");
+    postActionButtonsLike.setAttribute("id", "likeBtn");
 
-    let userSectionLogo = document.createElement('img')
-    userSectionLogo.classList.add('userSection__logo')
-    userSectionLogo.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzgv1fJtdWd-9_XAxYs5LeMewuX5USnZZ432pPfdmVGd5_9N6hSQ"); //cia bus userio logo
-    userSectionLogo.setAttribute("height", "50");
+    postAuthorSectionName.textContent = value.creator[0].userName;   //posto autoriaus userName
+    postAuthorSectionPostDate.textContent = value.date;   //posto autoriaus post date
+    postActionButtonsLike.innerHTML = '<ion-icon name="heart-empty"></ion-icon>'    //like mygtukas
+    postActionButtonsComment.innerHTML = '<ion-icon name="chatbubbles"></ion-icon>'    //comment mygtukas
+    postActionButtonsShare.innerHTML = '<ion-icon name="share"></ion-icon>'    //share mygtukas
+    postLikeCount.textContent = value.likesCount + ' likes';    //laiku skaicius
+    postCommentsListItemAuthorName.innerHTML = "Petras ".bold()   //posto autoriaus userName
+    postCommentsListItemAuthorComment.textContent = "nebūk ožys"   //posto autoriaus komentaras
+    postCommentsListItem2Username2.innerHTML = "John Cena ".bold()   //posto komentatoriaus userName1
+    postCommentsListItem2Username2Text2.textContent = "tu ozys"    //posto komentatoriaus komentaras1
+    postCommentsListItem3Username3.innerHTML = "Chuck ".bold()   //posto komentatoriaus userName2
+    postCommentsListItem3Username3Text3.textContent = "lopas"    //posto komentatoriaus komentaras2
+    postCreateCommentButton.textContent = "Post"
 
-    let userSectionName = document.createElement('span')
-    userSectionName.classList.add('userSection__name')
-    userSectionName.textContent = value.creator[0].userName   //cia bus userio name
-
-    let userSectionPostDate = document.createElement('span')
-    userSectionPostDate.classList.add('userSection__postDate')
-    userSectionPostDate.textContent = value.date;   //cia bus userio post date
-
-    userSection.appendChild(userSectionLogo)
-    userSection.appendChild(userSectionName)
-    userSection.appendChild(userSectionPostDate)
-
-    let photo = document.createElement('img')
-    photo.setAttribute("src", value.photo);   //cia yra posto nuotrauka
-    photo.setAttribute("width", "100%");
-
-    let actionButtons = document.createElement('div')   //mygtuku divas
-    actionButtons.classList.add('actionButtons')
-
-    let actionButtonsLike = document.createElement('span')
-    actionButtonsLike.classList.add('likeHeart')
-    actionButtonsLike.innerHTML = '<ion-icon name="heart-empty"></ion-icon>'    //like mygtukas
-
-    let actionButtonsComment = document.createElement('span')
-    actionButtonsComment.innerHTML = '<ion-icon name="chatbubbles"></ion-icon>'    //comment mygtukas
-
-    let actionButtonsShare = document.createElement('span')
-    actionButtonsShare.innerHTML = '<ion-icon name="share"></ion-icon>'    //share mygtukas
-
-    actionButtons.appendChild(actionButtonsLike)
-    actionButtons.appendChild(actionButtonsComment)
-    actionButtons.appendChild(actionButtonsShare)
-
-    let likeCount = document.createElement('div')
-    likeCount.classList.add('likeCount')
-    likeCount.textContent = value.likesCount + ' likes';   //cia laiku skaicius
-
-    let postDescription = document.createElement('div')
-    postDescription.classList.add('postDescription')
-    let postDescriptionUl = document.createElement('ul')
-
-    let postDescriptionAuthorLi = document.createElement('li')
-    let postDescriptionAuthorName = document.createElement('span')
-    postDescriptionAuthorName.innerHTML = "Petras ".bold()   //cia posto autoriaus userName
-    let postDescriptionAuthorComment = document.createElement('span')
-    postDescriptionAuthorComment.textContent = "nebūk ožys"   //cia posto autoriaus komentaras
-
-    postDescriptionAuthorLi.appendChild(postDescriptionAuthorName)
-    postDescriptionAuthorLi.appendChild(postDescriptionAuthorComment)
-
-    let postDescriptionComment = document.createElement('li')
-    let postDescriptionCommentUsername = document.createElement('span')
-    postDescriptionCommentUsername.innerHTML = "John Cena ".bold()   //cia posto autoriaus userName
-    let postDescriptionCommentText = document.createElement('span')
-    postDescriptionCommentText.textContent = "tu ozys"    //cia posto kometarai
-    postDescriptionComment.appendChild(postDescriptionCommentUsername)
-    postDescriptionComment.appendChild(postDescriptionCommentText)
-
-
-    let postDescriptionComment2 = document.createElement('li')
-    let postDescriptionCommentUsername2 = document.createElement('span')
-    postDescriptionCommentUsername2.innerHTML = "Chuck ".bold()   //cia posto autoriaus userName
-    let postDescriptionCommentText2 = document.createElement('span')
-    postDescriptionCommentText2.textContent = "lopas"    //cia posto kometarai
-    postDescriptionComment2.appendChild(postDescriptionCommentUsername2)
-    postDescriptionComment2.appendChild(postDescriptionCommentText2)
-
-
-    postDescription.appendChild(postDescriptionUl)
-    postDescription.appendChild(postDescriptionAuthorLi)
-    postDescription.appendChild(postDescriptionComment)
-    postDescription.appendChild(postDescriptionComment2)
-
-
-    let addComment = document.createElement('div')
-    addComment.classList.add('createCommentContainer', 'col', 'col-md-8')
-
-    let commentInput = document.createElement('input')
-    // commentInput.style.width: = "100%"
-
-    let commentPostButton = document.createElement('button')
-    commentPostButton.classList.add('addComment')
-    commentPostButton.textContent = "Post"
-
-    addComment.appendChild(commentInput)
-    addComment.appendChild(commentPostButton)
-
-
-    post.appendChild(userSection)
-    post.appendChild(photo)
-    post.appendChild(actionButtons)
-    post.appendChild(likeCount)
-    post.appendChild(postDescription)
-    post.appendChild(addComment)
     feed.appendChild(post)
-  });
+    post.appendChild(postAuthorSection)
+    postAuthorSection.appendChild(postAuthorSectionLogo)
+    postAuthorSection.appendChild(postAuthorSectionName)
+    postAuthorSection.appendChild(postAuthorSectionPostDate)
+    post.appendChild(postPhoto)
+    post.appendChild(postActionButtons)
+    postActionButtons.appendChild(postActionButtonsLike)
+    postActionButtons.appendChild(postActionButtonsComment)
+    postActionButtons.appendChild(postActionButtonsShare)
+    post.appendChild(postLikeCount)
+    post.appendChild(postComments)
+    postComments.appendChild(postCommentsList)
+    postCommentsList.appendChild(postCommentsListItem)
+    postCommentsList.appendChild(postCommentsListItem2)
+    postCommentsList.appendChild(postCommentsListItem3)
+    postCommentsListItem.appendChild(postCommentsListItemAuthorName)
+    postCommentsListItem.appendChild(postCommentsListItemAuthorComment)
+    postCommentsListItem2.appendChild(postCommentsListItem2Username2)
+    postCommentsListItem2.appendChild(postCommentsListItem2Username2Text2)
+    postCommentsListItem3.appendChild(postCommentsListItem3Username3)
+    postCommentsListItem3.appendChild(postCommentsListItem3Username3Text3)
+    post.appendChild(postCreateComment)
+    postCreateComment.appendChild(postCreateCommentInput)
+    postCreateComment.appendChild(postCreateCommentButton)
+
+    postActionButtonsLike.addEventListener('click', function() {
+      alert('here');
+    })
+
+  })
 };
